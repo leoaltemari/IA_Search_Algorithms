638709d7de07fe55c49bd9ab715df236415c34b7
leoaltemari <leo.altemari@usp.br>
2020-05-27 15:49:54 -0300

    saving changes

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ Nome: Leonardo Altemari Nogueira - 10284883
 // Configuring the server
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('./public'));
  
+// Classes
+var mapClass = require('./public/map');
 
  // Routes
      // Default GET
@@ -21,20 +23,20 @@ app.get('/', (req, res) => {
     res.sendFile('./public/index.html');
 });
 
-
-var mapClass = require('./public/map');
-var map = new mapClass.Map();
-
-try {
-    map.fullMap = fs.readFileSync('map.txt', 'utf8');
-    console.log(map.fullMap);    
-} catch(e) {
-    console.log('Error:', e.stack);
+function openFile(mapa) {
+    try {
+        var m = fs.readFileSync('./public/map.txt', 'utf8');
+        // console.log(a);    
+    } catch(e) {
+        console.log('Error:', e.stack);
+    }
+    return m;
 }
 
+var map = new mapClass.Map("");
 
-
-
+map.setFullMap(openFile(map));
+console.log(map.toString());
 
 
 // Run the server
--- public/map.js
+++ public/map.js
@@ -1,9 +1,16 @@
 'uses strict'
+
 class Map {
     fullMap;
     sourcePos;
     destPos;
 
+    setFullMap(_fullmap){
+        this.fullMap = _fullmap;
+    }
+    toString() {
+        return this.fullMap;
+    }
     getSourcePos() {
         return this.sourcePosPos;
     }
